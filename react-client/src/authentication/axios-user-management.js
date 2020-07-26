bd50aab3f6e78d7e4270c9d1f8a9dcf61b485430
Daksh Patel <dk792765@dal.ca>
2020-07-26 20:01:22 -0300

    Update axios-user-management.js

--- react-client/src/authentication/axios-user-management.js
+++ react-client/src/authentication/axios-user-management.js
@@ -1,3 +1,10 @@
+// Author: Daksh Patel
+
+// This file is used to create the axios instance with pre-defined interceptor
+// containing the authorization header. It takes the auth token from local-storage,
+// for basic authentication, encodes it in base64 and adds the encoded string to 
+// the authorization header.
+
 import axios from 'axios';
 
 const axiosInstance = axios.create({
