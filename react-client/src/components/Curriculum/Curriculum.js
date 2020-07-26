e19a3a1f8a2f43009434066c1bdcba8e9c70afc6
Mansoor Ghazi <mansoor.ghazi91@gmail.com>
2020-07-26 04:02:47 -0300

    Learning Path and auxillary features

---
+++ react-client/src/components/Curriculum/Curriculum.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Main from './Main';
+import Footer from '../Common/Footer';
+
+class Curriculum extends Component {
+    clearLocalStorage = () => {
+        localStorage.clear();
+        this.props.history.push('/');
+    }
+
+    render() {
+        return (
+            <>
+                <Header clearLocalStorage={this.clearLocalStorage} />
+                <Main projectID={this.props.match.params.projectID} />
+                <Footer />
+            </>
+        );
+    }
+}
+
+export default Curriculum;
