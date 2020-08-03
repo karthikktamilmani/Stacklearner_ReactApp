0c225a37e392f04dda31846cf5da37811eef8dd5
Daksh Patel <dk792765@dal.ca>
2020-08-03 16:28:01 -0300

    testing ForgotPassword.js

--- react-client/src/components/StudentDashboard/Project.js
+++ react-client/src/components/StudentDashboard/Project.js
@@ -2,7 +2,7 @@
 // Author: Ravi Patel
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Spinner from "react-bootstrap/Spinner";
 import {fadeInUp} from 'react-animations';
 import Radium, {StyleRoot} from 'radium';
@@ -26,20 +26,24 @@ const Project = ({_id, projectTitle, projectNumber, projectDemoLink, projectLeng
 							<div className={`project-card-image ${imageBackgroundColor}`}>
 					<span
 						className={projectAccessLevel.toLowerCase() === 'free' ? "project-access-level-label free-label-styles" : "project-access-level-label  pro-label-styles"}>{projectAccessLevel}</span>
-						<img src={projectImageURL} alt="Todo list app project"/>
-					</div>
-					<div className="project-card-outer-body">
-						<div className="progress-bar" style={{width: `${progress}%`}} role="progressbar" aria-valuenow={{progress}}
-								 aria-valuemin="0" aria-valuemax="100"/>
-						<div className="project-card-inner-body">
-							<h4>{projectTitle}</h4>
-							<div className="project-card-controls-container">
-								<Link
-									to={!isPro && projectAccessLevel.toLowerCase() === 'pro' ? "/subscription" : `/student/projects/${_id}`}
-									className="button button-small button-green-outline"><span>{progress === 0 ? "Start" : "Resume"}</span><i
-									className="fas fa-angle-right"/></Link>
-								<p
-									className="text-muted">Project {projectNumber} &middot; {projectLengthHours}h {projectLengthMinutes}m</p>
+								<img src={projectImageURL} alt="Todo list app project"/>
+							</div>
+							<div className="project-card-outer-body">
+								{/*{console.log(progress)}*/}
+								<div className="progress-bar" style={{width: `${progress}%`}} role="progressbar"
+										 aria-valuenow={{progress}}
+										 aria-valuemin="0" aria-valuemax="100"/>
+								<div className="project-card-inner-body">
+									<h4>{projectTitle}</h4>
+									<div className="project-card-controls-container">
+										<Link
+											to={!isPro && projectAccessLevel.toLowerCase() === 'pro' ? "/subscription" : `/student/projects/${_id}`}
+											className="button button-small button-green-outline"><span>{progress === 0 ? "Start" : "Resume"}</span><i
+											className="fas fa-angle-right"/></Link>
+										<p
+											className="text-muted">Project {projectNumber} &middot; {projectLengthHours}h {projectLengthMinutes}m</p>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
