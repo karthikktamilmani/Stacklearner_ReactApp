5f978eb0b5de0d6a599a461d45e0d187fbb75b1d
Daksh Patel <dk792765@dal.ca>
2020-08-03 17:37:01 -0300

    Removed images from public

--- express-server/models/discussion.model.js
+++ express-server/models/discussion.model.js
@@ -7,59 +7,59 @@
 // Import core dependencies
 const mongoose = require("mongoose");
 const {
-  Schema
+	Schema
 } = mongoose;
 
 
 const DiscussionSchema = Schema({
-  authorID: {
-    type: mongoose.Types.ObjectId,
-    ref: 'users',
-    required: true
-  },
-  content: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date
-  },
-  projectID: {
-    type: mongoose.Types.ObjectId,
-    ref: 'projects',
-    required: true
-  }
+	authorID: {
+		type: mongoose.Types.ObjectId,
+		ref: 'users',
+		required: true
+	},
+	content: {
+		type: String,
+		required: true
+	},
+	createdAt: {
+		type: Date
+	},
+	projectID: {
+		type: mongoose.Types.ObjectId,
+		ref: 'projects',
+		required: true
+	}
 });
 
 const LikesSchema = Schema({
-  discussionID: {
-    type: mongoose.Types.ObjectId,
-    ref: 'discussion',
-    required: true
-  },
-  userList: {
-    type: [mongoose.Schema.Types.ObjectId]
-  }
+	discussionID: {
+		type: mongoose.Types.ObjectId,
+		ref: 'discussion',
+		required: true
+	},
+	userList: {
+		type: [mongoose.Schema.Types.ObjectId]
+	}
 });
 
 const CommentsSchema = Schema({
-  discussionID: {
-    type: mongoose.Types.ObjectId,
-    ref: 'discussion',
-    required: true
-  },
-  authorID: {
-    type: mongoose.Types.ObjectId,
-    ref: 'users',
-    required: true
-  },
-  content: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date
-  }
+	discussionID: {
+		type: mongoose.Types.ObjectId,
+		ref: 'discussion',
+		required: true
+	},
+	authorID: {
+		type: mongoose.Types.ObjectId,
+		ref: 'users',
+		required: true
+	},
+	content: {
+		type: String,
+		required: true
+	},
+	createdAt: {
+		type: Date
+	}
 });
 
 const Discussion = mongoose.model("Discussion", DiscussionSchema, "discussion");
@@ -68,7 +68,7 @@ const Comments = mongoose.model("Comments", CommentsSchema, "comments");
 
 // Export models through object export pattern
 module.exports = {
-  Discussion,
-  Likes,
-  Comments
+	Discussion,
+	Likes,
+	Comments
 };
